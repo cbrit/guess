d200266ac298a08608af22f60e319aa11bc45d78
Collin Brittain <collinbrittain@gmail.com>
2021-08-14 15:32:44 -0500

    Ignore invalid input

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@ use rand::Rng;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
+    
+    println!("Guess the number!");
 
     loop {
-        println!("Guess the number! Input your guess.");
-
         let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        let guess: u32 = match guess.trim().parse() {
+            // num is the number result of the parse function.
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
